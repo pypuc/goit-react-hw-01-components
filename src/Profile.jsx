3055b97bc2ae18566147d3b24a7bc15335f3d908
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-10-04 16:48:28 +0300

    work

---
+++ src/Profile.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">ffollowers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
